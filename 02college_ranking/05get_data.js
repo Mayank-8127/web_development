0384bb7138f1317d583be1315c39f938d1351011
Mayank-8127 <145133998+Mayank-8127@users.noreply.github.com>
2024-08-19 22:49:40 +0530

    Get data about placements and faculty ratio

--- 02college_ranking/05get_data.js
+++ 02college_ranking/05get_data.js
@@ -2,23 +2,30 @@
 // #gender ratio,
 // #homestate students ratio other state students ratio foreign students,
 // #median placements,
-// #median placement average of 3 years,
 // #total students ratio placed students ratio students going for higher studies ratio unemployed students,
+// #student to faculty ratio
 
+// #median placement average of 3 years,
 // #money spent in last year
 // #money recieved for sponsered research and projects in last year
-// #student to faculty ratio
+
 
 const fs = require('fs');
 
 function checkCourseType(course){
-    a = ["PG [3","UG [3","UG [4","UG [5","PG [1","PG [2","PG [6","UG [6","PG-In"];
+    a = ["PG [3","UG [3","UG [4","UG [5"];
     for(let i = 0; i < a.length; i++){
-        if(course.split(":")[0] == a[i]){
-            return true;
+        if(course.slice(0,5) == a[i]){
+            return "long";
         }
     }
-    return false;
+    b = ["PG [1","PG [2","PG [6","UG [6","PG-In"]
+    for(let i = 0; i < b.length; i++){
+        if(course.slice(0,5) == b[i]){
+            return "short";
+        }
+    }
+    return "none";
 }
 
 data = fs.readFileSync('/home/mayank/repos/web_development/02college_ranking/11dataofpdfs.txt', 'utf-8');
@@ -27,7 +34,7 @@ let list = [];
 for(let i = 0; i < data.length; i++){
     if(data[i] == '<<>>'){
         let listoflist = [];
-        //listoflist.push(data[i+5]);
+        listoflist.push(data[i+5]);
         i += 14;
         courses = 0;
         while(data[i] != 'Total Actual Student Strength (Program(s) Offered by your Institution)'){
@@ -50,16 +57,51 @@ for(let i = 0; i < data.length; i++){
             course = data[i].slice(0,5);
             if(course == "PG-In"){
                 i--;
-                listoflist[1][course] = [data[i+2],data[i+3],data[i+4],data[i+5],data[i+6],data[i+7]];
+                listoflist[2][course] = [data[i+2],data[i+3],data[i+4],data[i+5],data[i+6],data[i+7]];
                 i += 14;
             }
             else{
-                listoflist[1][course] = [data[i+2],data[i+3],data[i+4],data[i+5],data[i+6],data[i+7]];
+                listoflist[2][course] = [data[i+2],data[i+3],data[i+4],data[i+5],data[i+6],data[i+7]];
                 i += 14;
             }
         }
-        i -= 1;
-        //listoflist.push(data[i]);
+        i++;
+        while(true){
+            let coursetype = checkCourseType(data[i]);
+            let course = data[i].slice(0,5);
+            if(coursetype == "none"){
+                break;
+            }else if(coursetype == "long"){
+                while(data[i] != "2022-23"){
+                    i++;
+                }
+                listoflist[2][course].push(data[i-3], data[i-1], data[i+1], data[i+2], data[i+3].split("(")[0]);
+                i += 3;
+                while(isNaN(data[i])){
+                    i++;
+                }
+                listoflist[2][course].push(data[i]);
+                i++;
+            }else if(coursetype == "short"){
+                while(data[i] != "2022-23"){
+                    i++;
+                }
+                listoflist[2][course].push(data[i-1], data[i+1], data[i+2], data[i+3].split("(")[0]);
+                i += 3;
+                while(isNaN(data[i])){
+                    i++;
+                }
+                listoflist[2][course].push(data[i]);
+                i++;
+            }else{
+                break;
+            }
+        }
+        while(data[i] != "Number of faculty members entered"){
+            i++;
+        }
+        listoflist.push(data[i+1]);
+
         list.push(listoflist);
     }
 }
